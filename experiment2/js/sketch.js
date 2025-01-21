7a1c0828a094276fa3a8179ef90674d895df2a53
jawshlev <soybr3ad@gmail.com>
2025-01-20 19:42:54 -0800

    Adding video to experiment2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,29 +1,27 @@
-// sketch.js - purpose and description here
+// sketch.js - Displays a rotating rectangle with a video background
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
 // Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
 const VALUE1 = 1;
 const VALUE2 = 2;
+let vid;
+let playing = true;
 
 // Globals
 let myInstance;
 let canvasContainer;
-var centerHorz, centerVert;
+let centerHorz, centerVert;
 
 class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+  constructor(param1, param2) {
+    this.property1 = param1;
+    this.property2 = param2;
+  }
 
-    myMethod() {
-        // code to run when method is called
-    }
+  myMethod() {
+    // Add any desired behavior here
+  }
 }
 
 function resizeScreen() {
@@ -31,30 +29,35 @@ function resizeScreen() {
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
 }
 
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
+  // Place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
+  // Resize canvas when the page is resized
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
+
+  // Load the video
+  vid = createVideo("img\angelshalo.mp4"); // Use forward slashes for file paths
+  vid.size(720, 1200); // Set video resolution
+  vid.volume(0); // Mute the video
+  vid.loop(); // Play the video in a loop
+  vid.hide(); // Hide the default video element
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
+  background(220);
+
+  // Display the video on the canvas
+  image(vid, (width - 720) / 2, (height - 1200) / 2, 720, 1200);
+
+  // Call a method on the instance
   myInstance.myMethod();
 
   // Set up rotation for the rectangle
@@ -73,7 +76,12 @@ function draw() {
   text("p5*", centerHorz - 105, centerVert + 40);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  // Toggle video playback on mouse press
+  if (playing) {
+    vid.pause();
+  } else {
+    vid.play();
+  }
+  playing = !playing;
 }
